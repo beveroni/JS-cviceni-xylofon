14afb116b0afab5db797f85f64eadfc5f066b17e
Veronika Bendova <svechovaver@gmail.com>
2023-07-29 21:22:42 +0200

    Task done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Funguju')
+// console.log('Funguju');
+const kameny = document.querySelectorAll('.kamen');
+const nadpis = document.querySelector('h1');
+
+const prehrajTony = (kamen) => {
+  const ton = kamen.textContent;
+  nadpis.textContent = ton;
+
+  const zvuk = new Audio(`tony/${ton}.mp3`);
+  zvuk.play();
+};
+
+const stisknuti = (udalost) => {
+  prehrajTony(udalost.target);
+};
+
+kameny.forEach((kamen) => {
+  kamen.addEventListener('click', stisknuti);
+});
+
+const stiskNaKlavesu = (e) => {
+  // const code = e
+  // if (code === (Digit))
+};
+document.addEventListener('keydown', stiskNaKlavesu);
+
+// Přidej ovládání přes klávesnici.
+// Přidej posluchač události keydown na celou stránku.
+// Pokud vlastnost code vzniklé události začíná na Digit, ulož si do proměnné následující číslo. Například z Digit4 si ulož 4.
+// Pokud je číslo větší nebo rovno jedné a menší nebo rovno počtu kamenů, přehraj tón na příslušném kamenu. Nezapomeň, že pole se v JavaScriptu indexují od nuly, takže například pro číslo 1 přehraj tón na kameni s indexem 0.
